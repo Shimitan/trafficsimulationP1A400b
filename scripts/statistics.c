39105a48cbe305158ba1b063a3dbc357d560973a
Arthur Osnes Gottlieb <arthurosnes@gmail.com>
2020-12-01 21:01:31 +0100

    Update statistics.c

--- scripts/statistics.c
+++ scripts/statistics.c
@@ -4,9 +4,9 @@
 
 /* 1000 målinger / sekund
  * 60 * 1000 målinger / minut
- * 60000 * 4 bytes
- * 240000 bytes
- * 240 kB / minuts data
+ * 60000 * 8 bytes
+ * 480000 bytes
+ * 480 kB / minuts data
  *
  * */
 
@@ -18,15 +18,17 @@
  *      - Baseret på 4 15 minutters målinger
  *
  * Data for hvert døgn? Maybe(TM)
- *
+ *      - Jeg har ingen ide om hvorvidt det er en god ide
  * */
 
 double* createSpeedArray(int amountOfCars, int ticksPerSecond){
-    double *speedArray = (double *) malloc(SECONDS_PER_MINUTE * amountOfCars * ticksPerSecond);
+    double *speedArray = (double *) malloc(SECONDS_PER_MINUTE * amountOfCars * ticksPerSecond * sizeof(double));
     return speedArray;
 }
 
+void analyseData(data oneMinuteData, ){
 
+}
 
 
 
